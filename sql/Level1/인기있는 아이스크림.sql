c171b59ab0fcdf296ad70ec39355d2e9a7287ecb
yeneua <rladPsk1117@gmail.com>
2023-07-13 18:16:16 +0900

    7/13

---
+++ sql/Level1/인기있는 아이스크림.sql
@@ -0,0 +1,6 @@
+-- 인기있는 아이스크림
+-- Q. 상반기에 판매된 아이스크림의 맛을 총주문량을 기준으로 내림차순 정렬하고 총주문량이 같다면 출하 번호를 기준으로 오름차순 정렬하여 조회하는 SQL 문을 작성해주세요.
+
+SELECT flavor
+from first_half
+order by total_order desc, shipment_id asc;
