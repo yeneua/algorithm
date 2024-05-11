d12bb1f25acbd639fbe823f4c817e98ffd8f3afc
yena <rladpsk1117@gmail.com>
2024-05-11 15:47:10 +0900

    120807 - 숫자 비교하기

---
+++ javascript/Level0/120807.js
@@ -0,0 +1,11 @@
+// 120807 - 숫자 비교하기
+function solution(num1, num2) {
+  var answer = 0;
+  if (num1 != num2) {
+    answer = -1;
+    return answer;
+  } else {
+    answer = 1;
+    return answer;
+  }
+}
