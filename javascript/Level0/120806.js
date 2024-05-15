83ce4afa6fe3a374a5d1443295bb3d9f6b608f4f
yena <rladpsk1117@gmail.com>
2024-05-15 23:01:04 +0900

    120806 - 두 수의 나눗셈

---
+++ javascript/Level0/120806.js
@@ -0,0 +1,7 @@
+// 두 수의 나눗셈
+function solution(num1, num2) {
+  var answer = 0;
+  return (answer = Math.floor((num1 / num2) * 1000));
+}
+
+const solution = (num1, num2) => Math.floor((num1 / num2) * 1000);
