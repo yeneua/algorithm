7cb96f46930500c186928f61fbd27ddb95163fa8
yena <rladpsk1117@gmail.com>
2025-01-28 16:36:37 +0900

    2019 - 더블더블

---
+++ SWEA/D1/2019.py
@@ -0,0 +1,6 @@
+# 더블더블
+
+number = int(input())
+
+for i in range(number+1):
+    print(2 ** i, end=' ')
--- SWEA/D1/2029.py
+++ SWEA/D1/2029.py
@@ -1,3 +1,7 @@
 # 몫과 나머지 출력하기
 
+T = int(input())
 
+for test_case in range(1, T+1):
+    operand1, operand2 = map(int, input().split())
+    print(f'#{test_case} {operand1//operand2} {operand1%operand2}')
