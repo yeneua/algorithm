19d50881f9712b980c4143bd243952f293056d69
Yena Kim <yeneua.dev@gmail.com>
2025-03-12 00:34:12 +0900

    2072 - 홀수만 더하기

--- SWEA/D1/2072.py
+++ SWEA/D1/2072.py
@@ -9,3 +9,11 @@ for test_case in range(T):
         if num % 2:
             result += num
     print(f'#{test_case + 1} {result}')
+
+
+# 두 번째 풀이
+T = int(input())
+
+for tc in range(1, T+1):
+    result = [x for x in list(map(int, input().split())) if x % 2 == 1]
+    print(f'#{tc}', sum(result))
